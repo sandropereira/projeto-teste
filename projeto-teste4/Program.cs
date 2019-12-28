15ad11af1b67c31a3794b63456ff339408f21531
sandropereira <sandrounib@gmail.com>
2019-12-28 19:56:11 -0300

    Acrescentado a mensagem Boa noite!

--- projeto-teste4/Program.cs
+++ projeto-teste4/Program.cs
@@ -4,6 +4,8 @@ namespace projeto_teste4 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa noite!");
+            Console.ReadLine();
         }
     }
 }
